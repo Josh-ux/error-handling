e8b70bfee0f8786d9de9eebc01a26f37d6cb7a94
Josh-ux <52693978+Josh-ux@users.noreply.github.com>
2024-11-19 19:09:10 +0300

    Update errorhandling.sol

--- errorhandling.sol
+++ errorhandling.sol
@@ -4,17 +4,17 @@ pragma solidity ^0.8.0;
 contract ClubMembership {
     mapping(address => bool) public members;
 
-  
+   //informs on when a user changes their membership
     event MembershipChanged(address member, bool isMember);
 
-    
+    //allows new users to register for club membership
     function joinClub() public {       
         require(!members[msg.sender], "Caller is already a member of the club.");
         members[msg.sender] = true;
         emit MembershipChanged(msg.sender, true);
     }
 
-  
+  //members can use this function to leave the club
     function leaveClub() public {
         if (!members[msg.sender]) {
             revert("Caller is not a member of the club.");
@@ -22,11 +22,13 @@ contract ClubMembership {
         members[msg.sender] = false;
         emit MembershipChanged(msg.sender, false);
     }
-    
+
+    //Allows a user to check their membership
     function checkMembership(address user) public view returns (bool) {
         return members[user];
     }
 
+//Used to assert that a member belongs to the club
     function validateMembership() public view {       
         assert(members[msg.sender] == true || members[msg.sender] == false);
     }
